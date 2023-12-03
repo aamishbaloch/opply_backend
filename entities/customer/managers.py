acfe599ea75170b9d137d27c21cdd3afc4e94360
Aamish Baloch <aamish.baloch@gmail.com>
2023-12-03 19:49:02 +0100

    [customer] Add unit tests for customer model

--- entities/customer/managers.py
+++ entities/customer/managers.py
@@ -1,20 +1,26 @@
 from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import UserManager
 from django.utils.translation import gettext_lazy as _
 
 
-class CustomerManager(BaseUserManager):
+class CustomerManager(UserManager):
 
-    def create_user(self, email, password, **extra_fields):
+    def _create_user(self, username, email, password, **extra_fields):
         extra_fields.update({
             "is_staff": False,
             "is_superuser": False,
             "is_active": False,
         })
 
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save()
-        return user
+        return super(CustomerManager, self)._create_user(username, email, password, **extra_fields)
 
-    def create_superuser(self, username, password, **extra_fields):
+    def create(self, **obj_data):
+        obj_data.update({
+            "is_staff": False,
+            "is_superuser": False,
+            "is_active": False,
+        })
+        return super(CustomerManager, self).create(**obj_data)
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
         raise ValueError(_("Customer cannot be a superuser"))
--- entities/customer/tests.py
+++ entities/customer/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Customer
+
+
+class CustomerTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_customer_cannot_be_a_staff_user(self):
+        params = {
+            'username': 'Zohaan',
+            'password': 'aamish',
+            'is_staff': True,
+        }
+
+        customer = Customer.objects.create(**params)
+
+        self.assertEqual(customer.username, 'Zohaan')
+        self.assertFalse(customer.is_staff)
+
+    def test_customer_cannot_be_a_super_user(self):
+        params = {
+            'username': 'Zohaan',
+            'password': 'aamish',
+            'is_superuser': True,
+        }
+
+        customer = Customer.objects.create(**params)
+
+        self.assertEqual(customer.username, 'Zohaan')
+        self.assertFalse(customer.is_staff)
